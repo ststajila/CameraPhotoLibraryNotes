fda2263dc1504eaade3a29ac695b8279f0cdfaa3
Stanislav <stazilastas@gmail.com>
2023-11-09 08:50:19 -0600

    Photo and Camera Notes

--- CameraPhotoLibraryNotes/ViewController.swift
+++ CameraPhotoLibraryNotes/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var imageViewOutlet: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
-
-
+    @IBAction func photoAction(_ sender: Any) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraAction(_ sender: Any) {
+    
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        } else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) {
+            self.imageViewOutlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        }
+    }
 }
 
